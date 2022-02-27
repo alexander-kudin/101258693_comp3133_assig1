39b976cdca794cfaaa2121f36b1738fafbf6ab20
Aleksandr Kudin <alexanderkudin@icloud.com>
2022-02-27 07:42:40 +0300

    Initial commit

---
+++ src/typeDefs/bookingTypeDefs.js
@@ -0,0 +1,30 @@
+import { gql } from "apollo-server-express";
+
+export const bookingTypeDefs = gql`
+
+  type Query {
+    getUserBookings: [Booking]
+  }
+
+  type Mutation {
+    createBooking(bookingInput: BookingInput): Booking
+  }
+
+  type Booking {
+    id: ID
+    listing_id: ID
+    booking_date: String
+    booking_start: String
+    booking_end: String
+    username: String
+  }
+
+  input BookingInput {
+    listing_id: ID!
+    booking_date: String!
+    booking_start: String!
+    booking_end: String!
+  }
+
+`;
+
